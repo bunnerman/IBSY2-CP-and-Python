ae62946281e089041f49db33ef0a13bfaa2141e9
bunnerman <parnilrbalawade@gmail.com>
2026-03-03 18:10:12 +0530

    Refactor second max element logic in C++

--- 4A Second Max Element.cpp
+++ 4A Second Max Element.cpp
@@ -11,22 +11,21 @@ int main()
 	for (int i = 0; i < n; i++)
 		cin >> ary[i];
 	
-	int max = ary[0];
-	int max2 = ary[1]; // second max
-
-	for (int i = 0; i < n; i++)
+	int max = ary[0] > ary[1] ? ary[0] : ary[1];
+	int max2 = ary[0] < ary[1] ? ary[0] : ary[1];
+	for (int i = 2; i < n; i++)
 	{
 		if (ary[i] > max)
 		{
 			max2 = max;
 			max = ary[i];
 		}
-		else if (ary[i] < max && ary[i] > max2)
+		else if (ary[i] > max2)
 			max2 = ary[i];
 	}
 
 	cout << "Max: " << max << "\n";
 	cout << "2nd max: " << max2;
 
-	return 0;		
+	return 0;
 }
