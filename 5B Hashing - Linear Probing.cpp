60bb476db596130edfaacc6128c46c225781d9c2
bunnerman <parnilrbalawade@gmail.com>
2026-03-19 00:33:08 +0530

    Update and rename 5BA Hashing - Linear Probing FLAWED INTERPRETATION.cpp to 5B Hashing - Linear Probing.cpp

---
+++ 5B Hashing - Linear Probing.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter " << n << " elements: ";
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+
+    int size = 5; // H(x) = x % size, better for demonstrating Linear Probing
+    vector<int> hashTable(size, -1);
+
+    for (int i = 0; i < n; i++)
+    {
+        int value = vec[i];
+        int h = value % size;
+        int loopDetectVar = h;
+
+        while (hashTable[h] != -1 && hashTable[h] != value)
+        {
+            h = (h + 1) % size;
+            if (h == loopDetectVar)
+                break;
+        }
+
+        if (hashTable[h] == value) // duplicates mapped to same
+            continue;
+        else if (hashTable[h] == -1)
+            hashTable[h] = value;
+        else
+            cout << "HASH TABLE FULL, " << value << " NOT INSERTED\n";
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << i << ": ";
+        if (hashTable[i] == -1)
+            cout << "---" << "\n";
+        else
+            cout << hashTable[i] << "\n";
+    }
+
+    return 0;
+}
