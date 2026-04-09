b767be4f52bf5c43c63a8f4575657d5fb022ad2d
bunnerman <parnilrbalawade@gmail.com>
2026-04-09 17:21:57 +0530

    Create 7E nth Smallest.cpp

---
+++ 7E nth Smallest.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+
+using namespace std; 
+
+int nthSmallest(int arr[], int n, int k)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)
+			max = arr[i];
+		int freq[max + 1] = {0};
+
+		for (int i = 0; i < n; i++)
+			freq[arr[i]]++;
+
+		int count = 0; 
+		
+		for (int i = 0; i <= max; i++)
+			if (freq[i] != 0)
+			{
+				count += freq[i];
+				if (count >= k)
+					return i;
+			}
+	}
+	return -1;
+}
+
+int main()
+{
+	int arr[] = {12, 3, 5, 7, 10};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int k = 2;
+	cout << nthSmallest(arr, n, k);
+
+	return 0;
+}
