4d64e13d75b1281949ac885b2e2f5c38a8d1ac5a
bunnerman <parnilrbalawade@gmail.com>
2026-04-09 17:28:23 +0530

    Create 8A Palindrome Checker.cpp

---
+++ 8A Palindrome Checker.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string str) 
+{
+	int left = 0, right = str.length() - 1;
+	while (left < right) 
+	{
+		if (str[left] != str[right])
+			return false;
+		left++;
+		right--;
+	}
+	return true;
+}
+
+int main() 
+{
+	string str;
+	cout << "Enter a string: ";
+	cin >> str;
+	if (isPalindrome(str))
+		cout << "Is Palindrome";
+	else
+		cout << "Isn't Palindrome";
+}
