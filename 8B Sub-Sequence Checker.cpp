a07dccf2a2e9a8f6a594b7695bc50d1f7384e77b
bunnerman <parnilrbalawade@gmail.com>
2026-04-09 17:44:39 +0530

    Create 8B Sub-Sequence Checker.cpp

---
+++ 8B Sub-Sequence Checker.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool isSubSeq(string s1, string s2)
+{
+	int m = s1.size();
+	int n = s2.size();
+	
+	int j = 0;
+	for (int i = 0; i < n && j < m; i++)
+		if(s2[i] == s1[j])
+			j++;
+	return j == m;
+}
+
+int main() 
+{
+	string s1, s2;
+	cin >> s1; 
+	cin >> s2;
+	cout << boolalpha << isSubSeq(s1, s2);
+
+	return 0;
+}
